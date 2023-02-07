2f366c701749c11d8441dd880461184cd9c7b201
Mumtahin Farabi <mfarabi619@gmail.com>
2023-02-06 20:09:46 -0500

    Create responsive linear gradient background for light theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Background from "./Components/Background.jsx";
+import Background from "./Components/Background/Background.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
