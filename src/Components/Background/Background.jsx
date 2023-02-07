7ddb0a6901b2d96b269098742d17c9932d4af084
Mumtahin Farabi <mfarabi619@gmail.com>
2023-02-06 19:57:58 -0500

    Move files for background into Background folder

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Background.scss";
 
 function Background() {
-  return <div className="Background">Some text</div>;
+  return <div className="background">Some text</div>;
 }
 
 export default Background;
