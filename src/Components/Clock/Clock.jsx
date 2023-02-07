3ceb9fd5fde188b74f9b9d2c5325afe6f596fb2f
Mumtahin Farabi <mfarabi619@gmail.com>
2023-02-06 21:49:49 -0500

    Create sections for break and session length

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./Clock.scss";
 
 function Clock() {
-  return <div className="Container">Some text</div>;
+  return (
+    <div className="Container">
+      <section className="break">
+        <p className="break-label">Break Length</p>
+      </section>
+
+      <section className="session">
+        <p className="session-label">Session Length</p>
+      </section>
+    </div>
+  );
 }
 
 export default Clock;
