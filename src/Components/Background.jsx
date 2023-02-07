dd049c01b6dfb9d2edfefa3cbcd4ff60742b9718
Mumtahin Farabi <mfarabi619@gmail.com>
2023-02-06 19:47:48 -0500

    Delete boilerpalte and fix imports for Background

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./Background.css";
 
 function App() {
   return <div className="App">Some text</div>;
