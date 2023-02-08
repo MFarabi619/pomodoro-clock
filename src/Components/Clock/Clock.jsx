2e9bf8ba3740e500ad8e539b69bf430e748114d6
Mumtahin Farabi <mfarabi619@gmail.com>
2023-02-07 21:06:33 -0500

    Create spans for increment and decrement buttons

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -10,10 +10,14 @@ function Clock() {
       <div className="container-2">
         <section className="break-container">
           <p id="break-label">Break Length</p>
+          <span id="break-increment"></span>
+          <span id="break-decrement"></span>
         </section>
 
         <section className="session-container">
           <p id="session-label">Session Length</p>
+          <span id="session-increment"></span>
+          <span id="session-decrement"></span>
         </section>
       </div>
     </div>
